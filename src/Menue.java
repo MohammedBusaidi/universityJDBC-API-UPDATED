eccc3e7a2fe0c856de69383ef1d6b9fd80ebbd75
mohammed albusaidi <moody.busaidi99@gmail.com>
2023-04-03 21:46:14 +0400

    created thee menue class using hashMap

--- src/Menue.java
+++ src/Menue.java
@@ -1,2 +1,36 @@
+import java.util.HashMap;
+import java.util.Scanner;
+
 public class Menue {
+    Scanner menueSc = new Scanner(System.in);
+
+    public void showMenue() {
+        boolean menueLoop = true;
+        while (menueLoop) {
+            HashMap<Integer, String> menueOptions = new HashMap<>();
+            menueOptions.put(1, "Initialize Database");
+            menueOptions.put(2, "Search the name of the country to fetch universites");
+            menueOptions.put(3, "Get list of countries");
+            menueOptions.put(4, "Insert the Data");
+            menueOptions.put(5, "Backup Database");
+            menueOptions.put(6, "Remove Table");
+            menueOptions.put(7, "Show all universites");
+            menueOptions.put(8, "fetch Data");
+            menueOptions.put(9, "Search by: ");
+            menueOptions.put(10, "Dump data into file");
+            menueOptions.put(11, "Retrive data From file");
+            menueOptions.put(12, "Exit");
+
+            int choice = 0;
+            while (choice != 12) {
+                System.out.println("==================UNIVERSITIES DATABASE==================");
+                for (int i = 1; i <= 12; i++) {
+                    System.out.println(i + ". " + menuOptions.get(i));
+                }
+                System.out.println("=========================================================");
+                System.out.print("Enter your choice: ");
+            }
+        }
+
+    }
 }
